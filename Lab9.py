086d3f576d26aa9e9db0e20a45996cb714116b76
masonconfer <masonconfer00@gmail.com>
2014-11-06 14:00:57 -0600

    LastCommit

---
+++ Lab9.py
@@ -0,0 +1,19 @@
+############################################
+#                                          # 
+#                   70pt                   #
+#                                          #
+############################################
+
+# Create a celcius to fahrenheit calculator.
+# Multiply by 9, then divide by 5, then add 32 to calculate your answer.
+
+# TODO:
+# Ask user for Celcius temperature to convert
+# Accept user input 
+# Calculate fahrenheit
+# Output answer
+
+print "Please input tempurature in celcius"
+celcius = int(raw_input())
+fahrenheit = celcius * 9 / 5 + 32
+print fahrenheit
